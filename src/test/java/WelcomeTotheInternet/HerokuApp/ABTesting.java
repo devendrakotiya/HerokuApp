df877ffb4ea3d6bb64072d68fe403b336a647bcc
devendrakotiya <devendrakotiya01@gmail.com>
2025-02-08 16:14:02 +0530

    third commit

--- src/test/java/WelcomeTotheInternet/HerokuApp/ABTesting.java
+++ src/test/java/WelcomeTotheInternet/HerokuApp/ABTesting.java
@@ -16,8 +16,7 @@ public class ABTesting {
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Thread.sleep(2000);
 		driver.close();
-		
-		System.out.println("Hey git how are you");		
+			
 	}
 
 }
